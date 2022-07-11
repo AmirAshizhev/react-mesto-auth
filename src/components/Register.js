b46591dc86bade35378cb8be18f3a8244b93cc83
Amir Ashizhev <amirashizhev@yandex.ru>
2022-07-11 19:32:52 +0300

    feat: register submit check

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,29 +1,49 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const Register = () => {
+
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleChange(e) {
+    const {name, value} = e.target;
+    setData((oldData) => ({
+      ...oldData,
+      [name]: value
+    }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log (data)
+  }
+
   return (
-    <form className="login">
+    <form className="login" onSubmit={handleSubmit}>
     <h2 className="login__title">Регистрация</h2>
     <fieldset className="login__fieldset">
       <label className="popup__field">
         <input 
-          id="" 
           type="email"  
           name="email" 
           placeholder="Email" 
           className="login__input" 
           required 
+          onChange={handleChange}
         />
         <span className="popup__item-error edit-button-name-error"></span>
       </label>
       <label className="popup__field">
         <input 
-          id="" 
           type="password"  
           name="password" 
           placeholder="Пароль" 
           className="login__input" 
           required 
+          onChange={handleChange}
         />
         <span className="popup__item-error edit-button-description-error"> </span>
       </label>
